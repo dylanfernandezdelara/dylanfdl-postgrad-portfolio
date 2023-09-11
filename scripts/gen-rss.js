447a24000637e2f368999fc03093e2d013c97b08
dylanfernandezdelara <dylanfernandezdelara@yale.edu>
2023-09-11 00:26:47 -0500

    configured portfolio basics

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Dylan Fernandez de Lara',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
